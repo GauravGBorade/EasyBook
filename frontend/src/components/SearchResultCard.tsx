a7dfab39d127a63141669c814c32dff0bc6a0f96
Gaurav Borade <gauravborade618@gmail.com>
2024-03-10 16:37:44 +0530

    fixed: Mobile Layout Fixed

--- frontend/src/components/SearchResultCard.tsx
+++ frontend/src/components/SearchResultCard.tsx
@@ -1,12 +1,29 @@
 import { AiFillStar } from "react-icons/ai";
 import { HotelType } from "../../../backend/src/shared/types";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 type Props = {
   hotel: HotelType;
 };
 const SearchResultCard = ({ hotel }: Props) => {
   let starCount = 1;
+  const [facilitiesToShow, setFacilitiesToShow] = useState(
+    window.innerWidth < 640 ? 2 : 3
+  );
+  useEffect(() => {
+    const handleResize = () => {
+      setFacilitiesToShow(window.innerWidth < 640 ? 2 : 3);
+    };
+
+    // Attach the event listener
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <div className="grid grid-cols-1 xl:grid-cols-[2fr_3fr] border border-slate-300 rounded-lg sm:p-8 p-4 gap-8">
@@ -42,14 +59,16 @@ const SearchResultCard = ({ hotel }: Props) => {
         </div>
         <div className="grid grid-col-1 sm:grid-cols-2 items-end whitespace-nowrap">
           <div className="flex gap-1 items-center">
-            {hotel.facilities.slice(0, 3).map((facility, index) => (
-              <span
-                key={`facility-${index}`}
-                className="bg-gray-500	text-white p-2 rounded-lg text-xs whitespace-nowrap"
-              >
-                {facility}
-              </span>
-            ))}
+            {hotel.facilities
+              .slice(0, facilitiesToShow)
+              .map((facility, index) => (
+                <span
+                  key={`facility-${index}`}
+                  className="bg-gray-500	text-white p-2 rounded-lg text-xs whitespace-nowrap"
+                >
+                  {facility}
+                </span>
+              ))}
             <span className="text-sm">
               {hotel.facilities.length > 3 &&
                 `+${hotel.facilities.length - 3} more`}
